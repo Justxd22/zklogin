e208e5b80c68f2eae86ca8dc837405d092ee086b
justxd22 <xdjust18@gmail.com>
2025-03-22 15:53:16 +0200

    fiX

--- src/main.rs
+++ src/main.rs
@@ -517,7 +517,7 @@ async fn main() -> std::io::Result<()> {
             .route("/", web::get().to(index))
             .route("/live", web::get().to(ping))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
